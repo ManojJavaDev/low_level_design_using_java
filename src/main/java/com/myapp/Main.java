e0a9d445bcb0b64a22d1b0cf585d71abd3749c53
Manoj kumar <tech.manoj.madireddy@gmail.com>
2025-02-25 20:54:00 -0600

    initial working directory setup

--- src/main/java/com/myapp/Main.java
+++ src/main/java/com/myapp/Main.java
@@ -2,6 +2,6 @@ package com.myapp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("initial working directory set up completed and connected to remote repo");
     }
 }
